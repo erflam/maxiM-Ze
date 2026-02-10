3256108cb1918ce26b807074e3800e3098ab65f5
erflam <elizabethflammer@ufl.edu>
2026-02-10 15:58:57 -0500

    commit

--- Config.py
+++ Config.py
@@ -5,7 +5,7 @@ class Config:
     BASE_DIR = Path.home() / "Desktop/maxiMiZe Tests"
     INPUT_SUBDIR = Path("maxiMiZe Files")
     OUTPUT_ROOT = Path("maxiMiZe Checkpoints")
-    ANALYSIS_FOLDER = "maxiMZe Tests 0210 12"
+    ANALYSIS_FOLDER = "maxiMZe Tests 0210 15"
 
     # Analysis parameters
     MASS_GROUPS = {
